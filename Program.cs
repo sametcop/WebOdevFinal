0edff074190afe0c45aa40cebe30be495d5772da
sametcop <16008124039@ogr.bozok.edu.tr>
2025-12-26 07:55:09 +0300

    Switch database from SQL Server to SQLite

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+ options.UseSqlite("Data Source=app.db");
 
 
 builder.Services.AddIdentity<AppUser, IdentityRole>(options =>
@@ -78,3 +78,4 @@ app.MapControllerRoute(
 
 
 app.Run();
+
